418df5720fae654bf9daf582e07f65f914cd13a0
valiantProgrammer <rupayandey134@gmail.com>
2026-01-26 12:41:39 +0530

    1.0.1.6

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -44,7 +44,7 @@ class DataValidationConfig:
         self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
         self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir,training_pipeline.TEST_FILE_NAME)
         self.drift_report_file_path: str = os.path.join(
-            self.data_validation_dir,
-            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
-            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+                self.data_validation_dir,
+                training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+                training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
             )
