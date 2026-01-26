b7940dc5232fb9de70e349a69cecbf6edf51e4a8
valiantProgrammer <rupayandey134@gmail.com>
2026-01-26 20:42:34 +0530

    1.0.1.20

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -16,10 +16,35 @@ from networksecurity.entity.config_entity import DataTransformationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.utils.main_utils.utils import save_numpy_array_data, save_object
+import pandas as pd
+import numpy as np
 
 class DataTransformation:
-    def __init__(self, data_validation_artifact:DataValidationArtifact):
+    def __init__(self,
+                 data_validation_artifact:DataValidationArtifact,
+                 data_transformation_config:DataTransformationConfig
+                 ):
         try:
             self.data_validation_artifact=data_validation_artifact
+            self.data_transformation_config=data_transformation_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def read_data(file_path: str) -> pd.Dataframe:
+     
+    def initiate_data_transformation(self)->DataTransformationArtifact:
+        logging.info("Entered initiate data transformation")
+        try:
+            logging.info("Starting Data Transformation")
+            train_df=DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df=DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+            ## trainning dataframe
+            input_feature_train_df=train_df.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_train_array=train_df[TARGET_COLUMN]
+            target_feature_train_array = target_feature_train_array.replace(-1, 0)
+            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_test_array = test_df[TARGET_COLUMN]
+            target_feature_test_array = target_feature_test_array.replace(-1, 0)
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
