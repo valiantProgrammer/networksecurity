a6d4e88fb1833e043d8ffb53f9b4b86a51444d92
valiantProgrammer <rupayandey134@gmail.com>
2026-01-26 21:51:45 +0530

    1.0.1.25

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -62,3 +62,14 @@ class DataTransformationConfig:
         )
         self.transformed_object_file_path:str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
                                                              training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
+        
+        
+        
+class ModelTrainerConfig:
+    def __init__(self,training_pipeline_config: TrainingPipelineConfig):
+        self.model_trainer_dir:str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_TRAINER_DIR_NAME
+        )
+        self.trained_model_file_path:str = os.path.join(
+            self.model_trainer_dir,training_pipeline.M
+        )
