3e059e9d05c7212cdd42fff37cbcfd04cb9c2344
valiantProgrammer <rupayandey134@gmail.com>
2026-01-26 20:19:40 +0530

    1.0.1.17

---
+++ networksecurity/components/data_transformation.py
@@ -0,0 +1,19 @@
+import sys 
+import os
+import numpy as np
+import pandas as dp
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from networksecurity.constant.training_pipeline import TARGET_COLUMN
+from networksecurity.constant.training_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
+
+from networksecurity.entity.artifact_entity import (
+    DataTransformationArtifact,
+    DataValidationArtifact
+)
+from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+
