1ceca23f041c0e8c836ae1ad2d18ac6010a62a1a
valiantProgrammer <rupayandey134@gmail.com>
2026-01-26 20:26:08 +0530

    1.0.1.18

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -15,5 +15,7 @@ from networksecurity.entity.artifact_entity import (
 from networksecurity.entity.config_entity import DataTransformationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import save_numpy_array_data, save_object
+
 
 
