4a5199ed0ccf5166449d38fc20e7cd5ff80c86c1
valiantProgrammer <rupayandey134@gmail.com>
2026-01-27 13:46:58 +0530

    1.0.2.7

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 mongo_db_url = os.getenv("MONGODB_URL")
 print(mongo_db_url)
+
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
