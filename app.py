ec6f43faabd63489ce41ff8ef5c9e53d9a59cea4
valiantProgrammer <rupayandey134@gmail.com>
2026-02-10 16:28:06 +0530

    1.0.3.1.4

--- app.py
+++ app.py
@@ -9,23 +9,24 @@ print(mongo_db_url)
 
 import pymongo
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.logging.logger import logging
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, File, UploadFile, Request
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
 
-
 from networksecurity.utils.main_utils.utils import load_object
+from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME, DATA_INGESTION_DATABASE_NAME
 
 
 client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
-
-from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME, DATA_INGESTION_DATABASE_NAME
+templates = Jinja2Templates(directory="./templates")
 
 database = client[DATA_INGESTION_DATABASE_NAME]
 collection = database[DATA_INGESTION_COLLECTION_NAME]
@@ -53,6 +54,23 @@ async def train_model():
         return Response("Training is successfull")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
-    
+
+@app.get("predict")
+async def predict_route(request: Request, file:UploadFile=File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        preprocessor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor,model=final_model)
+        print(df.iloc[0])
+        y_pred=network_model.predict(df)
+        df['predicted_column'] = y_pred
+        print(df["predicted_column"])
+        df.to_csv("prediction_output/output.csv")
+        table_html = df.to_html(classes='table table-striped')
+        
+        return templates.TemplateResponse("table.html", {"request": request,"table": table_html})
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
 if __name__ == "__main__":
     app_run(app, host="localhost", port=8000)
